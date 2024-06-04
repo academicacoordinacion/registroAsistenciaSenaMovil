0a532043842972cc9e3eae6e8ae67f08b4b04a1c
Williamlo21 <lopezrebellonw@gmail.com>
2024-06-04 16:41:48 -0500

    screen reponsive

--- lib/screens/dashboard_screens.dart
+++ lib/screens/dashboard_screens.dart
@@ -1,18 +1,17 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:registro_asistencia_sena_movil/models/ambiente_response.dart';
-import 'package:registro_asistencia_sena_movil/models/departamento_response.dart';
 import 'package:registro_asistencia_sena_movil/models/ficha_caracterizacion_response.dart';
 import 'package:registro_asistencia_sena_movil/models/login_response.dart';
 import 'package:registro_asistencia_sena_movil/screens/fichas_caracterizacion/index1_screens.dart';
 import 'package:registro_asistencia_sena_movil/services/app_services.dart';
-import 'package:registro_asistencia_sena_movil/utils/constantes.dart';
 import 'package:registro_asistencia_sena_movil/widgets/footer.dart';
 import 'package:registro_asistencia_sena_movil/widgets/header.dart';
 
 class DashboardScreen extends StatefulWidget {
-  const DashboardScreen({super.key, required this.loginResponse});
+  const DashboardScreen({Key? key, required this.loginResponse})
+      : super(key: key);
+
   final LoginResponse loginResponse;
 
   @override
@@ -21,59 +20,72 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   final AppServices appServices = AppServices();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(MediaQuery.of(context).size.height *
-            0.1), // Establecer la altura del AppBar como el 10% del alto de la pantalla
+        preferredSize:
+            Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
         child: Header(loginResponse: widget.loginResponse),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Card(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                // width: 350,
-                // height: 300,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Padding(padding: EdgeInsets.all(10)),
-                      Text(
-                        "Bienvenido nuevamente, ${widget.loginResponse.persona.primerNombre} ${widget.loginResponse.persona.primerApellido}!",
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Padding(padding: EdgeInsets.all(10)),
+                        Text(
+                          "Bienvenido nuevamente, ${widget.loginResponse.persona.primerNombre} ${widget.loginResponse.persona.primerApellido}!",
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      const Text(
-                          "Aquí podras tomar la asistencia de los aprendices en los días de formacón ."),
-                      const Divider(
-                        color: Colors.grey,
-                        thickness: 2,
-                        indent: 20,
-                        endIndent: 20,
-                      ),
-                      const Text(
-                          "Recuerda que primero debes ingresar la ficha de caracterización y luego escanear el código QR para tomar la asistencia a la hora de la entrada y de la salida."),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          apiIndex(widget.loginResponse.persona.instructorId,
-                              widget.loginResponse.persona.regionalId, context);
-                        },
-                        child: const Text('Comencemos'),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        const Text(
+                          "Aquí podrás tomar la asistencia de los aprendices en los días de formación.",
+                          textAlign: TextAlign.justify,
+                        ),
+                        const Divider(
+                          color: Colors.grey,
+                          thickness: 2,
+                          indent: 20,
+                          endIndent: 20,
+                        ),
+                        const Text(
+                          "Recuerda que primero debes ingresar la ficha de caracterización y luego escanear el código QR para tomar la asistencia a la hora de la entrada y de la salida.",
+                          textAlign: TextAlign.justify,
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              apiIndex(
+                                  widget.loginResponse.persona.instructorId,
+                                  widget.loginResponse.persona.regionalId,
+                                  context);
+                            },
+                            child: const Text('Comencemos'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: const footer(),
@@ -84,12 +96,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
   void apiIndex(int instructorId, int regionalID, BuildContext context) async {
     try {
       final ambientes = await apiCargarAmbientes(regionalID);
-      // print("estos son los ambientes");
-      // print(ambientes);
       final fichasCaracterizacion =
           await apiCargarFichasCaracterizacion(instructorId);
-        //   print("estas son las fichas");
-        // print(fichasCaracterizacion);
       if (ambientes != null && fichasCaracterizacion != null) {
         Navigator.push(
           context,
@@ -119,11 +127,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
     }
   }
 
-
   Future<List<Ambiente>?> apiCargarAmbientes(int regionalID) async {
     final data = await appServices.getAmbientes(regionalID);
     return data.isNotEmpty ? data : null;
   }
+
   Future<List<FichaCaracterizacion>?> apiCargarFichasCaracterizacion(
       int instructorId) async {
     final data = await appServices.getFichasCaracterizacion(instructorId);
