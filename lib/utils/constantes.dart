15de7372aabf7af0f7bb4aaaa69f22b2ee4d1876
Williamlo21 <lopezrebellonw@gmail.com>
2024-06-26 16:17:12 -0500

    url constantes

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,5 +1,6 @@
 class Constantes {
   // static const baseUrl = 'http://192.168.3.234:8000/api';
-  // static const baseUrl = 'http://10.230.80.160:8000/api';
-  static const baseUrl = 'http://10.0.2.2:8000/api';//ruta para acceder al localhost
+  static const baseUrl = 'http://10.230.80.121:8000/api';
+  // static const baseUrl = 'http://10.0.2.2:8000/api';//ruta para acceder al localhost
+
 }
